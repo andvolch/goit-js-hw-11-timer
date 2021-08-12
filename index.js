397c49496041269d0cbc32d1c9d15d4ba05c2865
Andrey Volchenskiy <andvolch@gmail.com>
2021-08-12 14:41:12 +0300

    add files

---
+++ index.js
@@ -0,0 +1,48 @@
+// Таймер
+const timer = {
+  start() {
+    const startTime = new Date('Jul 17, 2019');
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      console.log(`${days}:${hours}:${mins}:${secs}`);
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+};
+
+function getTimeComponents(time) {
+  /*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+};
